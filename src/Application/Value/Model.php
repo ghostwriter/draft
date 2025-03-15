91db7985237287d55f37d9a2757a0a79a48483e1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-03-15 18:14:04 -0500

    Move Model.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Application/Value/Model.php
+++ src/Application/Value/Model.php
@@ -2,42 +2,44 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Draft\Value;
+namespace Ghostwriter\Draft\Application\Value;
 
 use Closure;
-use Ghostwriter\Draft\Contract\MigrationInterface;
-use Ghostwriter\Draft\Contract\ModelInterface;
+use Ghostwriter\Draft\Application\Interface\MigrationInterface;
+use Ghostwriter\Draft\Application\Interface\ModelInterface;
 use Illuminate\Database\Eloquent\Model as IlluminateModel;
 use Illuminate\Support\Str;
+use Override;
 
 final class Model implements ModelInterface
 {
-    private ?MigrationInterface $migration = null;
+    private ?IlluminateModel $illuminateModel = null;
 
-    private ?IlluminateModel $model = null;
+    private ?MigrationInterface $migration = null;
 
     private ?string $table = null;
 
     public function __construct(
-        private string $name
-    ) {
-    }
+        private readonly string $name
+    ) {}
 
     public function controller(): string
     {
         return '';
     }
-//
-//    public function getForeignKey(): string
-//    {
-//        /** @var self->table */
-//        return Str::of($this->table)
-//            ->singular()
-//            ->snake()
-//            ->append('_' . $this->getKeyName())
-//            ->toString();
-//    }
 
+    //
+    //    public function getForeignKey(): string
+    //    {
+    //        /** @var self->table */
+    //        return Str::of($this->table)
+    //            ->singular()
+    //            ->snake()
+    //            ->append('_' . $this->getKeyName())
+    //            ->toString();
+    //    }
+
+    #[Override]
     public function migration(): MigrationInterface
     {
         return $this->migration ??= new Migration($this);
@@ -45,28 +47,31 @@ final class Model implements ModelInterface
 
     public function model(): IlluminateModel
     {
-        return $this->model ??= new class() extends IlluminateModel {
-        };
+        return $this->illuminateModel ??= new class() extends IlluminateModel {};
     }
 
+    #[Override]
     public function name(): string
     {
         return $this->name;
     }
 
+    #[Override]
     public function namespace(): string
     {
         return '';
     }
 
+    #[Override]
     public function table(): string
     {
         return $this->table ??= Str::of($this->name)->plural()->lower()->toString();
     }
 
+    #[Override]
     public function withMigration(?Closure $factory = null): void
     {
-        if (null === $factory) {
+        if (! $factory instanceof Closure) {
             return;
         }
 
