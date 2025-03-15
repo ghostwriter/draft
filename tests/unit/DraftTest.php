d7b48844cdc7ef74d597f00453db7f6ea03ac17a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-03-15 18:14:18 -0500

    Update DraftTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/DraftTest.php
+++ tests/unit/DraftTest.php
@@ -2,75 +2,146 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Draft\Tests\Unit;
+namespace Tests\Unit;
 
+use Ghostwriter\Draft\Application\Definition\ControllerDefinition;
+use Ghostwriter\Draft\Application\Definition\ModelDefinition;
+use Ghostwriter\Draft\Application\Formatter;
+use Ghostwriter\Draft\Application\NameProvider;
+use Ghostwriter\Draft\Application\PathProvider;
+use Ghostwriter\Draft\Application\Sanitizer;
+use Ghostwriter\Draft\Console\Command\BuildCommand;
+use Ghostwriter\Draft\Console\Command\InitCommand;
+use Ghostwriter\Draft\Console\Command\NewCommand;
+use Ghostwriter\Draft\Console\Command\TraceCommand;
+use Ghostwriter\Draft\Container\Factory\InflectorFactory;
+use Ghostwriter\Draft\Container\Factory\ParserFactory;
+use Ghostwriter\Draft\Container\ServiceProvider;
 use Ghostwriter\Draft\Draft;
 use Ghostwriter\Draft\DraftServiceProvider;
-use Illuminate\Container\Container;
-use Illuminate\Foundation\Application;
+use Ghostwriter\Draft\Parser\ClassMap;
+use Ghostwriter\Draft\Parser\FindControllers;
+use Ghostwriter\Draft\Parser\FindModels;
+use Ghostwriter\Draft\Parser\Node\DraftFileNode;
+use Ghostwriter\Draft\Parser\Printer;
+use Ghostwriter\Draft\Parser\Visitor\DraftVisitor;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\Attributes\UsesClass;
 
+#[CoversClass(BuildCommand::class)]
+#[CoversClass(ClassMap::class)]
+#[CoversClass(ControllerDefinition::class)]
 #[CoversClass(Draft::class)]
 #[CoversClass(DraftServiceProvider::class)]
-#[Small]
-final class DraftTest extends \Orchestra\Testbench\TestCase
+#[CoversClass(DraftVisitor::class)]
+#[CoversClass(FindControllers::class)]
+#[CoversClass(FindModels::class)]
+#[CoversClass(Formatter::class)]
+#[CoversClass(InitCommand::class)]
+#[CoversClass(ModelDefinition::class)]
+#[CoversClass(NewCommand::class)]
+#[CoversClass(ParserFactory::class)]
+#[CoversClass(ServiceProvider::class)]
+#[CoversClass(TraceCommand::class)]
+#[CoversClass(DraftFileNode::class)]
+#[CoversClass(Printer::class)]
+#[CoversClass(InflectorFactory::class)]
+#[CoversClass(Sanitizer::class)]
+#[CoversClass(NameProvider::class)]
+#[CoversClass(PathProvider::class)]
+#[CoversClass(NamespaceProvider::class)]
+final class DraftTest extends AbstractTestCase
 {
-    private Container $container;
-
-    private Draft $draft;
-
-    protected function setUp(): void
+    public function assertDraftHasControllerDefinitions(Draft $draft): void
     {
-        $this->container = new Container();
-        parent::setUp();
+        $modelDefinitions = $draft->controllerDefinitions();
 
-        $this->draft = $this->container->get(Draft::class);
+        self::assertNotEmpty($modelDefinitions);
+
+        self::assertContainsOnlyInstancesOf(ControllerDefinition::class, $modelDefinitions);
+    }
+
+    public function assertDraftHasModelDefinitions(Draft $draft): void
+    {
+        $modelDefinitions = $draft->modelDefinitions();
+
+        self::assertNotEmpty($modelDefinitions);
+
+        self::assertContainsOnlyInstancesOf(ModelDefinition::class, $modelDefinitions);
+    }
+
+    public function assertDraftIsEmpty(Draft $draft): void
+    {
+        self::assertEmpty($draft->controllerDefinitions());
+        self::assertEmpty($draft->eventDefinitions());
+        self::assertEmpty($draft->factoryDefinitions());
+        self::assertEmpty($draft->inertiaDefinitions());
+        self::assertEmpty($draft->jobDefinitions());
+        self::assertEmpty($draft->livewireDefinitions());
+        self::assertEmpty($draft->mailDefinitions());
+        self::assertEmpty($draft->migrationDefinitions());
+        self::assertEmpty($draft->modelDefinitions());
+        self::assertEmpty($draft->notificationDefinitions());
+        self::assertEmpty($draft->policyDefinitions());
+        self::assertEmpty($draft->routeDefinitions());
+        self::assertEmpty($draft->ruleDefinitions());
+        self::assertEmpty($draft->seederDefinitions());
+        self::assertEmpty($draft->testsDefinitions());
+        self::assertEmpty($draft->viewDefinitions());
+    }
+
+    public function testCommand(): void
+    {
+
+        //        $filesystem->expets('test');
+        //        $this->artisan(InitCommand::class)->assertSuccessful();
+        $this->artisan(TraceCommand::class, [])->assertSuccessful();
+
+        //        $this->artisan(BuildCommand::class, [])->assertSuccessful();
+        //        $this->artisan(NewCommand::class)
+        //            ->expectsQuestion('What should the model be named?', 'Post')
+        //            ->assertSuccessful();
     }
 
     public function testDraftControllersIsEmpty(): void
     {
-        self::assertEmpty($this->draft->controllers());
+        self::assertEmpty($this->draftVisitor->controllers());
     }
 
     public function testDraftFactoriesIsEmpty(): void
     {
-        self::assertEmpty($this->draft->factories());
+        self::assertEmpty($this->draftVisitor->factories());
+    }
+
+    public function testDraftHasControllerDefinitions(): void
+    {
+        $draft = Draft::new(static fn (Draft $draft) =>$draft->controller('Post', static fn () => null));
+
+        $this->assertDraftHasControllerDefinitions($draft);
+    }
+
+    public function testDraftHasModelDefinitions(): void
+    {
+        $draft = Draft::new(static fn (Draft $draft) =>$draft->model('Post', static fn () => null));
+
+        $this->assertDraftHasModelDefinitions($draft);
+    }
+
+    public function testDraftIsEmpty(): void
+    {
+        $draft = new Draft();
+
+        $this->assertDraftIsEmpty($draft);
     }
 
     public function testDraftModelsIsNeverEmpty(): void
     {
-        self::assertCount(1, $this->draft->models());
-        self::assertNotEmpty($this->draft->models());
+        self::assertCount(1, $this->draftVisitor->models());
+        self::assertNotEmpty($this->draftVisitor->models());
     }
 
     public function testDraftSeedersIsEmpty(): void
     {
-        self::assertEmpty($this->draft->seeders());
-    }
-
-    /**
-     * Define environment setup.
-     *
-     * @param Application $app
-     */
-    protected function defineEnvironment($app): void
-    {
-        $app->config->set('draft.debug', 'false');
-        $app->config->set('draft.default', [
-            'user' => '\App\Models\User',
-        ]);
-    }
-
-    /**
-     * Get package providers.
-     *
-     * @param Application $app
-     *
-     * @return array<int, class-string>
-     */
-    protected function getPackageProviders($app)
-    {
-        return [DraftServiceProvider::class];
+        self::assertEmpty($this->draftVisitor->seeders());
     }
 }
