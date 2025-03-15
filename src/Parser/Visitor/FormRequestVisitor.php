9603fe0138b6da549c9c9f28ec82da92ada6c000
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-03-15 18:14:14 -0500

    Move FormRequestVisitor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Parser/Visitor/FormRequestVisitor.php
+++ src/Parser/Visitor/FormRequestVisitor.php
@@ -2,9 +2,8 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Draft\Visitor;
+namespace Ghostwriter\Draft\Parser\Visitor;
 
-use Ghostwriter\Draft\Visitor\Traits\NodeVisitorTrait;
 use PhpParser\NodeVisitor;
 
 final class FormRequestVisitor implements NodeVisitor
