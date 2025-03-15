38a38b3838bb1727af512d01213d9a9483b01e48
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-03-15 18:14:00 -0500

    Add RoutePostDefinition.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Application/Definition/Router/Action/RoutePostDefinition.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Draft\Application\Definition\Router\Action;
+
+use Ghostwriter\Draft\Application\Interface\DefinitionInterface;
+
+final readonly class RoutePostDefinition implements DefinitionInterface
+{
+    public function __construct(
+        private string $route,
+        private string $handler,
+    ) {}
+}
