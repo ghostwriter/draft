c72ba9224cba1000a001ee7d9ad1a5feb982084f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-03-15 18:14:01 -0500

    Add FormRequestStatementDefinition.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Application/Definition/Statement/FormRequestStatementDefinition.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Draft\Application\Definition\Statement;
+
+use Ghostwriter\Draft\Application\Interface\Definition\StatementDefinitionInterface;
+
+final class FormRequestStatementDefinition implements StatementDefinitionInterface
+{
+    public function __construct(
+        private string $name,
+        private array $rules = []
+    ) {}
+}
