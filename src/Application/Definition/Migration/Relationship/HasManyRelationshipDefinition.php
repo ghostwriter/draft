faa95f059011f27a89bcba290ddea3c52e72f168
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-03-15 18:13:59 -0500

    Add HasManyRelationshipDefinition.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Application/Definition/Migration/Relationship/HasManyRelationshipDefinition.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Draft\Application\Definition\Migration\Relationship;
+
+final class HasManyRelationshipDefinition
+{
+    public function __construct(
+        private string $model,
+        private string $foreignKey,
+        private string $localKey,
+    ) {}
+}
