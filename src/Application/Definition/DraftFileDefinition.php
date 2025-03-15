442a64f99c6f1903929fa9111fad9127f1dd0d27
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-03-15 18:13:58 -0500

    Add DraftFileDefinition.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Application/Definition/DraftFileDefinition.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Draft\Application\Definition;
+
+use Ghostwriter\Draft\Application\Interface\DefinitionInterface;
+
+final class DraftFileDefinition implements DefinitionInterface {}
