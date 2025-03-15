e74512c6de1b09c5a5cccd6e4a135210a9a76db3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-03-15 18:13:59 -0500

    Add NotificationDefinition.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Application/Definition/NotificationDefinition.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Draft\Application\Definition;
+
+use Ghostwriter\Draft\Application\Interface\DefinitionInterface;
+
+final readonly class NotificationDefinition implements DefinitionInterface
+{
+    public function __construct(
+        private string $name,
+    ) {}
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+}
