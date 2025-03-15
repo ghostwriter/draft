92835fa6faeed232ec7edef060b25f9db899c29d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-03-15 18:14:02 -0500

    Move ActionInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Application/Interface/Controller/ActionInterface.php
+++ src/Application/Interface/Controller/ActionInterface.php
@@ -2,18 +2,19 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Draft\Contract\Controller;
+namespace Ghostwriter\Draft\Application\Interface\Controller;
 
-use Ghostwriter\Draft\Contract\ModelInterface;
+use Ghostwriter\Draft\Application\Interface\Definition\StatementDefinitionInterface;
+use Ghostwriter\Draft\Application\Interface\ModelInterface;
 
 interface ActionInterface
 {
     public function name(): string;
 
-    public function statement(StatementInterface $statement): self;
+    public function statement(StatementDefinitionInterface $statement): self;
 
     /**
-     * @return iterable<string,StatementInterface>
+     * @return iterable<string,StatementDefinitionInterface>
      */
     public function statements(): iterable;
 
