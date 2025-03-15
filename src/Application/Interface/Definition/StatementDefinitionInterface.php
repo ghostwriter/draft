cd745d60627502ed5bf0596635447a6e23fefd5d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-03-15 18:14:03 -0500

    Add StatementDefinitionInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Application/Interface/Definition/StatementDefinitionInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Draft\Application\Interface\Definition;
+
+interface StatementDefinitionInterface
+{
+    //    public function getId(): string;
+}
+// Statement
